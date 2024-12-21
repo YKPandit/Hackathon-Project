bdb43c6708b1ce0bce96498b37d5eee42c05f642
YKPandit <yaaskakpandit@gmail.com>
2024-12-21 11:34:28 -0800

    added the ui sprites

--- New_AWS_Project/obj/Debug/net8.0/New AWS Project.AssemblyInfo.cs
+++ New_AWS_Project/obj/Debug/net8.0/New AWS Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New AWS Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e16f95acfb2934edf956f572e633be393937f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1095b889402a73e07f0e220b665f7b6d83bcb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("New AWS Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New AWS Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
